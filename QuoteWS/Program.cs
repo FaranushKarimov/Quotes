cede1e617f6742cf8c70f64e66e0674c56351c14
Tima Taziev <timataziev@gmail.com>
2020-07-10 16:10:15 +0300

    Создание записи каждые 5 минут для Worker, которая удаляется после часа

---
+++ QuoteWS/Program.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+
+
+namespace QuotesWS
+{
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    using Microsoft.Extensions.Logging;
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices(services =>
+                {
+                    services.AddHostedService<Worker>();
+                });
+    }
+}
